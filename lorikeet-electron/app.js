303b717ae45810cf348a689286e3829239409600
luofei <502207456@qq.com>
2018-08-05 22:32:14 +0800

    实现了文件夹的浏览

--- lorikeet-electron/app.js
+++ lorikeet-electron/app.js
@@ -3,12 +3,18 @@
 
 const fileSystem = require('./fileSystem');
 const userInterface = require('./userInterface');
-
+const search = require('./search');
 
 function main(){
     userInterface.bindDocument(window);
     const folderPath = fileSystem.getUserHomeFolder();
+
     userInterface.loadDirectory(folderPath)(window);
+    userInterface.bindSearchField((event)=>{
+        const query = event.target.value;
+        if(query === '')userInterface.resetFilter();
+        else search.find(query, userInterface.filterResults);
+    });
 }
 
 window.onload = main;
--- lorikeet-electron/userInterface.js
+++ lorikeet-electron/userInterface.js
@@ -2,16 +2,21 @@
 
 let document;
 const fileSystem = require('./fileSystem');
+const path = require('path');
+const search = require('./search');
 
 function displayFile(file){
     const template = document.querySelector('#item-template');
     let clone = document.importNode(template.content, true);
-    clone.querySelector('img').src = `images/${file.type}.svg`;
+    let img = clone.querySelector('img');
+    img.src = `images/${file.type}.svg`;
+    img.setAttribute('data-filePath', file.path);
+    search.addToIndex(file);
 
     if(file.type == 'directory'){
-        clone.querySelector('img').addEventListener('dblclick', ()=>{
+        img.addEventListener('dblclick', ()=>{
             loadDirectory(file.path)();
-        });
+        }, false);
     }
     clone.querySelector('.filename').innerText = file.file;
     const mainArea = document.getElementById('main-area');
@@ -29,7 +34,8 @@ function bindDocument(window){
 }
 
 function displayFolderPath(folderPath){
-    document.getElementById('current-folder').innerText = folderPath;
+    document.getElementById('current-folder').innerHTML = convertFolderPathIntoLinks(folderPath);
+    bindCurrentFolderPath();
 }
 
 function clearView(){
@@ -44,6 +50,8 @@ function clearView(){
 function loadDirectory(folderPath){
     return function(window){
         if(!document)document = window.document;
+        search.resetIndex();
+
         displayFolderPath(folderPath);
         fileSystem.getFilesInFolder(folderPath, (err, files)=>{
             clearView();
@@ -53,4 +61,47 @@ function loadDirectory(folderPath){
     }
 }
 
-module.exports = {bindDocument, displayFiles, loadDirectory};
+function bindSearchField(cb){
+    document.getElementById('search').addEventListener('keyup', cb, false);
+}
+
+function filterResults(results){
+    const validFilePaths = results.map((result)=>{
+        return result.ref;
+    });
+    const items = document.getElementsByClassName('item');
+    for(var i = 0; i < items.length; ++i){
+        let item = items[i];
+        let filePath = item.getElementsByTagName('img')[0].getAttribute('data-filepath');
+        if(validFilePaths.indexOf(filePath) !== -1)item.style = null;
+        else item.style = 'display:none;';
+
+    }
+}
+
+/// 默认全部显示
+function resetFilter(){
+    const items = document.getElementsByClassName('item');
+    for(var i = 0; i < items.length; ++i)items[i].style = null;
+}
+
+function convertFolderPathIntoLinks(folderPath){
+    const folders = folderPath.split(path.sep);
+    const contents = [];
+    let pathAtFolder = '';
+    folders.forEach((folder)=>{
+        pathAtFolder += folder + path.sep;
+        contents.push(`<span class="path" data-path="${pathAtFolder.slice(0, -1)}">${folder}</span>`);
+    });
+    return contents.join(path.sep).toString();
+}
+
+function bindCurrentFolderPath(){
+    const load = (event)=>{
+        const folderPath = event.target.getAttribute('data-path');
+        loadDirectory(folderPath)();
+    };
+    const paths = document.getElementsByClassName('path');
+    for(var i = 0; i < paths.length; ++i)paths[i].addEventListener('click', load, false);
+}
+module.exports = {bindDocument, bindSearchField, displayFiles, filterResults, loadDirectory, resetFilter, };
